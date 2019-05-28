8385e4de06dc4edd47b768687b031bf05f2f5e7a
Caio-8 <caiorussellrox@msn.com>
2019-05-28 00:57:27 -0300

    Getting values of inputs

--- General/Exercices JS/helpjs-ravi-master/Exercises/Exercise 04/full-name.js
+++ General/Exercices JS/helpjs-ravi-master/Exercises/Exercise 04/full-name.js
@@ -2,12 +2,6 @@ $(function(){
     var input   = $("input[type='text']"),
         button  = $("input[type='button']");
     $(button).click(function (){
-<<<<<<< HEAD
         alert("O nome completo é: " +'"'+ input.val() +'"');
     });
-}); 
-=======
-        alert(input.value);
-    });
 });
->>>>>>> df0ce68188b488c9bd2204efc99a742b2cdd0f23
