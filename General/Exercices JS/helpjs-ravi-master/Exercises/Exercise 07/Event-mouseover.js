cb9bf0b9d63b737d25e57dfd77b0c49509d342bf
cdvlopr <caiorussellrox@msn.com>
2019-10-06 00:42:44 -0300

    lighting up and erasing lamp with the event mouseover

--- General/Exercices JS/helpjs-ravi-master/Exercises/Exercise 07/Event-mouseover.js
+++ General/Exercices JS/helpjs-ravi-master/Exercises/Exercise 07/Event-mouseover.js
@@ -1,12 +1,17 @@
 "use strict"
 
 function eventHover(){
-	var _button = $('figure .subtitle'),
+	var _button = $('figure .subtitle figcaption'),
 	_imgLamp = $('figure .countimg img'),
+	_imgLampOff = $('a').eq(0).attr('href'),
 	_imgLampOn = $('a').eq(1).attr('href')
 
-	_button.on('click', function(){
-		_imgLamp.attr('src', _imgLampOn)
+	_button.mouseover(function(){
+		if (_imgLamp.attr('src') == _imgLampOff) {
+			_imgLamp.attr('src', _imgLampOn);
+		} else if (_imgLamp.attr('src') == _imgLampOn) {
+			_imgLamp.attr('src', _imgLampOff);
+		}
 	})
 }
 
