69f76ff2d8dc8b2443ccf4fa858e8b0bb2b6e583
cdvlopr <caiorussellrox@msn.com>
2019-11-20 22:40:36 -0300

    now the users can't to send empty values.

--- General/Exercices JS/helpjs-ravi-master/Exercises/Exercise 16/two-parameters.js
+++ General/Exercices JS/helpjs-ravi-master/Exercises/Exercise 16/two-parameters.js
@@ -17,8 +17,12 @@ function validate(dom,tipo) {
 	switch(tipo){
 		case'num' : var regex = /[A-Za-z- ]/g;
 		break;
-		case'text' : var regex = /[\d-]/g;
+		case'text' : var regex =  /[\d]/g;
 		break;
+	}
+
+	if(dom.value === " "){
+		dom.value = dom.value.replace(/[ ]/g,'');
 	}
 
 	dom.value = dom.value.replace(regex,'');
@@ -32,10 +36,10 @@ function parameters(value1, value2) {
 	$letUp = value1.toLowerCase().split(" "),
 	$name = $letUp;
 
-	// for (var a = 0; a < $letUp.length; a++) {
-	// 	var w = $letUp[a];
-	// 	$letUp[a] = w[0].toUpperCase() + w.slice(1);
-	// }
+	for (var a = 0; a < $letUp.length; a++) {
+		var w = $letUp[a];
+		$letUp[a] = w[0].toUpperCase() + w.slice(1);
+	}
 
 	if($age === 0 || $age === 1){
 		$('#result').html(
@@ -43,17 +47,14 @@ function parameters(value1, value2) {
 			+'<br>'
 			+'Sendo assim você ainda é de menor.'
 			)
-	} else if($age < 18 && $age >= 0){
+	} else if($age < 18 && $age > 1){
 		$('#result').html(
 			'Olá ' +$name.join(" ")+ ', sua idade é de ' +$age+ ' anos.'
 			+'<br>'
-			+'Sendo assim você ainda é de menor.'
+			+'Sendo assim você ainda é menor de idade.'
 			)
 	} else if(value2 > $dateFullYear){
 		$('#result').html('Digite a data correta o jamanta!')
-		return false
-	} else if(!value1.trim() || !value2.trim()){
-		$('#result').html('Os campos não podem ser vazios, fdp!')
 		return false
 	} else {
 		$('#result').html(
