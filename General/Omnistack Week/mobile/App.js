bb54561904bc63115533d4d36efbeaeaa2605f84
caio-caldas <caio.caldas@agenciaeconverse.com.br>
2020-02-25 00:33:35 -0300

    feat: new configurations and add others necessary packages

--- General/Omnistack Week/mobile/App.js
+++ General/Omnistack Week/mobile/App.js
@@ -1,32 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Learning developer React-Native!</Text>
-      <Text style={styles.myName}>Caiozinho (;</Text>
-    </View>
+    <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-    color: '#fff',
-    fontSize: 12,
-    textTransform: 'uppercase',
-  },
-
-  myName: {
-    fontSize: 30,
-    color: 'gold',
-    fontWeight: 'bold',
-  },
-});
