a25b4803914d8f0b4061b896cfab23b812a432d6
caio-caldas <caio.caldas@agenciaeconverse.com.br>
2020-03-15 22:39:45 -0300

    feat: add resource to add new user

--- Courses/Alura/introducao-javascript/js/principal.js
+++ Courses/Alura/introducao-javascript/js/principal.js
@@ -28,10 +28,53 @@ for (var i = 0; i < pacientes.length; i++) {
     }
 
     if (alturaEhValida && pesoEhValido) {
-
-        var imc = peso / (altura * altura);
+        var imc = calculaImc(peso,altura);
         tdImc.textContent = imc.toFixed(0);
     } else {
-        tdImc.textContent = "Altura e/ou peso inválidos!"
+        tdImc.textContent = "Altura e/ou peso inválidos!";
     }
+}
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = calculaImc(peso,altura);
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+});
+
+function calculaImc(peso,altura){
+    var imc = 0;
+
+    imc = peso / (altura * altura);
+
+    return imc;
 }
