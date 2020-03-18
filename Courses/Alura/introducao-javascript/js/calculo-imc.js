f33b652c7aaaed5d89fe2dffc523195bb922fc58
caio-caldas <caio.caldas@agenciaeconverse.com.br>
2020-03-17 22:59:08 -0300

    feat: segregate the functions in little blocks

---
+++ Courses/Alura/introducao-javascript/js/calculo-imc.js
@@ -0,0 +1,44 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var altura = tdAltura.textContent;
+    var peso = tdPeso.textContent;
+
+    var alturaEhValida = true;
+    var pesoEhValido = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido!");
+        tdPeso.textContent = "Peso inválido!";
+        pesoEhValido = false;
+        paciente.classList.add('notificacao-erro');
+    }
+
+    if (altura <= 0 || altura >= 3) {
+        console.log("Altura inválida!");
+        tdAltura.textContent = "Altura inválida!";
+        alturaEhValida = false;
+        paciente.classList.add('notificacao-erro');
+    }
+
+    if (alturaEhValida && pesoEhValido) {
+        var imc = calculaImc(peso,altura);
+        tdImc.textContent = imc.toFixed(0);
+    } else {
+        tdImc.textContent = "Altura e/ou peso inválidos!";
+    }
+}
+
+function calculaImc(peso,altura){
+    var imc = 0;
+
+    imc = peso / (altura * altura).toFixed(0);
+
+    return imc;
+}
