f93bf656c9d765ed5e6825baa1bec21073d452f4
caio-caldas <caio.caldas@agenciaeconverse.com.br>
2020-03-29 00:32:08 -0300

    feat: bonus class

--- General/Rocketseat/Omnistack Week 11/aulas/backend/src/routes.js
+++ General/Rocketseat/Omnistack Week 11/aulas/backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 const OngController = require('./controllers/OngController');
 const incidentsController = require("./controllers/incidentsController");
 const ProfileController = require("./controllers/ProfileController");
@@ -23,10 +24,30 @@ const routes = express.Router();
 
 routes.post("/sessions", SessionController.create);
 routes.get("/ongs", OngController.index);
-routes.post("/ongs", OngController.create);
-routes.get("/profile", ProfileController.index);
-routes.get("/incidents", incidentsController.index);
+routes.post("/ongs", celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}), OngController.create);
+routes.get("/profile", celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+}), ProfileController.index);
+routes.get("/incidents", celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+    })
+}), incidentsController.index);
 routes.post("/incidents", incidentsController.create);
-routes.delete("/incidents/:id", incidentsController.delete);
+routes.delete("/incidents/:id", celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}), incidentsController.delete);
 
 module.exports = routes;
