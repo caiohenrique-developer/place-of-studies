e0ee2f136b7bf4b624cc08c807422b2e9ed1de57
caio-caldas <caio.caldas@agenciaeconverse.com.br>
2020-03-30 22:37:53 -0300

    feat: searching data inside api

--- Courses/Alura/fetch-api/clientData.js
+++ Courses/Alura/fetch-api/clientData.js
@@ -1,31 +1,28 @@
-const clientData = [
-    {
-        cpf: 41539228894,
-        name: "Eu mesmo"
-    },
-    {
-        cpf: 41539228894,
-        name: "Outro usuário"
-    }
-]
+fetch('http://localhost:4000/clientes')
+.then(response => {
+    return response.json();
+}).then(teste => {
+    console.log(teste);
+})
 
-const table = document.querySelector("[data-table]");
 
-const showClient = (cpf, name) => {
-    const newLine = document.createElement('tr');
+// const table = document.querySelector("[data-table]");
+
+// const showClient = (cpf, name) => {
+//     const newLine = document.createElement('tr');
     
-    const data = `
-        <td>${cpf}</td>
-        <td>${name}</td>
-    `
+//     const data = `
+//         <td>${cpf}</td>
+//         <td>${name}</td>
+//     `
 
-    newLine.innerHTML = data;
+//     newLine.innerHTML = data;
 
-    return newLine;
-}
+//     return newLine;
+// }
 
-clientData.forEach(indice => {
-    console.log(indice);
+// clientData.forEach(indice => {
+//     console.log(indice);
     
-    table.appendChild(showClient(indice.cpf, indice.name));
-});
+//     table.appendChild(showClient(indice.cpf, indice.name));
+// });
