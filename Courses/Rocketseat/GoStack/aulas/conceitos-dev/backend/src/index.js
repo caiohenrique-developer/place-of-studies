52726021f498e62a3282c6bb1ffbe481e010de1e
caio-caldas <caio.caldas@agenciaeconverse.com.br>
2020-04-07 22:42:14 -0300

    feat: now i learned some params types, used to identify routes, filters and paginations, request body contents

--- Courses/Rocketseat/GoStack/aulas/conceitos-dev/backend/src/index.js
+++ Courses/Rocketseat/GoStack/aulas/conceitos-dev/backend/src/index.js
@@ -2,7 +2,15 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/projects', (request, response) => {
+  const { model, color, userName } = request.query;
+  
+  console.log(model);
+  console.log(color);
+  console.log(userName);
+
   return response.json([
     'projeto 1',
     'projeto 2',
@@ -11,6 +19,12 @@ app.get('/projects', (request, response) => {
 });
 
 app.post('/projects', (request, response) => {
+  const { project_name, agency_name, squad } = request.body;
+
+  console.log(project_name);
+  console.log(agency_name);
+  console.log(squad);
+
   return response.json([
     'projeto 1',
     'projeto 2',
@@ -21,6 +35,18 @@ app.post('/projects', (request, response) => {
 
 // localhost:3333/projects/1
 app.put('/projects/:id', (request, response) => {
+  // route params
+  const { id } = request.params;
+  // request body
+  const { project_name, client_name, agency_name, squad } = request.body;
+
+  console.log(`id: ${id}`);
+
+  console.log(`Nome do projeto: ${project_name}`);
+  console.log(`Nome da cliente: ${client_name}`);
+  console.log(`Agência: ${agency_name}`);
+  console.log(`Setor: ${squad}`);
+
   return response.json([
     'projeto 8',
     'projeto 2',
