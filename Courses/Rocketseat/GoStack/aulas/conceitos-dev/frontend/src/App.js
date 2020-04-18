f7259bcd4a072193976e2306ece05f4b952f98e5
caio-caldas <caio.caldas@agenciaeconverse.com.br>
2020-04-18 04:55:32 -0300

    feat: i learned important concepts about how connect frontend whit backend and get values

--- Courses/Rocketseat/GoStack/aulas/conceitos-dev/frontend/src/App.js
+++ Courses/Rocketseat/GoStack/aulas/conceitos-dev/frontend/src/App.js
@@ -1,28 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
+import api from './services/api';
 import './App.css';
 import backgroundImage from './assets/modelo-cafe-da-tarde.jpg';
 
 function App() {
-  const [ projects, setProjects ] = useState( ['Dev web', 'Dev mob'] );
+  // change value of component
+  const [ projects, setProjects ] = useState( [] );
 
+  // connection whit backend
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+      console.log(response.data);
+    });
+  }, []);
+
+  // add value to component
   function handleAddProject() {
     setProjects( [...projects, `Novo projeto ${Date.now()}`] );
   }
 
+  // return html with contents
   return (
     <>
       <Header title="Home" />
 
       <img src={backgroundImage} width={200} />
         
-      <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
+      {/* <button type="button" onClick={handleAddProject}>Adicionar Projeto</button> */}
 
       <ul>
         {
+          // traverses the object and returns a html
           projects.map(project => {
             return (
-              <li key={project}>{project}</li>
+              <li key={project.id}>{project.project_name}</li>
             )
           })
         }
