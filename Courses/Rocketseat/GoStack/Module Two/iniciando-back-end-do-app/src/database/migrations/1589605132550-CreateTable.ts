59a003ca7f11f2a0505cf493f27e9ac09fad80f9
caio-caldas <caio.caldas@agenciaeconverse.com.br>
2020-05-16 02:03:14 -0300

    feat: feature to add values to data base with typeorm developed

---
+++ Courses/Rocketseat/GoStack/Module Two/iniciando-back-end-do-app/src/database/migrations/1589605132550-CreateTable.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateTable1589605132550 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
