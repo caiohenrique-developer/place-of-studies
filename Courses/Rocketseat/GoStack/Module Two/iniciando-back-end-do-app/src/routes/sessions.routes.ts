9a88b56d88c77542d17226f3a0d1116669dea9ec
caio-caldas <caio.caldas@agenciaeconverse.com.br>
2020-05-20 23:50:08 -0300

    feat: handling known and unknown errors

--- Courses/Rocketseat/GoStack/Module Two/iniciando-back-end-do-app/src/routes/sessions.routes.ts
+++ Courses/Rocketseat/GoStack/Module Two/iniciando-back-end-do-app/src/routes/sessions.routes.ts
@@ -5,22 +5,18 @@ import AuthenticateUserService from '../services/AuthenticateUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-	try {
-		const { email, password } = request.body;
+	const { email, password } = request.body;
 
-		// create a instance with my service
-		const authenticateUser = new AuthenticateUserService();
+	// create a instance with my service
+	const authenticateUser = new AuthenticateUserService();
 
-		// return a response of the method execute of my service
-		const { user, token } = await authenticateUser.execute({ email, password });
+	// return a response of the method execute of my service
+	const { user, token } = await authenticateUser.execute({ email, password });
 
-		// delete password visibility on frontend
-		delete user.password;
+	// delete password visibility on frontend
+	delete user.password;
 
-		return response.json({ user, token });
-	} catch (err) {
-		return response.status(err.statusCode).json({ error: err.message });
-	}
+	return response.json({ user, token });
 });
 
 export default sessionsRouter;
