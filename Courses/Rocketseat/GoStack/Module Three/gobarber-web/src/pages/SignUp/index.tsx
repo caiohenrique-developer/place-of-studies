99df7fac407bc72af2dbbdb946ee0139dadf788d
caiohenrique-developer <caiorussellrox@msn.com>
2020-07-06 21:03:11 -0300

    :feat: register page done :sparkles:

--- Courses/Rocketseat/GoStack/Module Three/gobarber-web/src/pages/SignUp/index.tsx
+++ Courses/Rocketseat/GoStack/Module Three/gobarber-web/src/pages/SignUp/index.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FiMail, FiUser, FiLock, FiArrowLeft } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
-const Signin: React.FC = () => (
+const SignUp: React.FC = () => (
   <Container>
+    <Background />
     <Content>
       <img src={logoImg} alt="Logotipo GoBarber" />
 
       <form>
-        <h1>Faça seu logon</h1>
+        <h1>Faça seu cadastro</h1>
 
+        <Input icon={FiUser} name="name" placeholder="Nome" />
         <Input icon={FiMail} name="email" placeholder="E-mail" />
         <Input
           icon={FiLock}
@@ -21,18 +23,15 @@ const Signin: React.FC = () => (
           placeholder="Senha"
         />
 
-        <Button type="submit">Entrar</Button>
-
-        <a href="forgot">Esqueci minha senha</a>
+        <Button type="submit">Cadastrar</Button>
       </form>
 
       <a href="login">
-        <FiLogIn />
-        Criar conta
+        <FiArrowLeft />
+        Voltar para logon
       </a>
     </Content>
-    <Background />
   </Container>
 );
 
-export default Signin;
+export default SignUp;
