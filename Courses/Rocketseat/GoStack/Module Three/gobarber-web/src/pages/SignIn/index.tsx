f3f58e0baa11089983aa96c01ec8ef54d3873d3f
caiohenrique-developer <caiorussellrox@msn.com>
2020-07-19 17:53:13 -0300

    [FEAT]: save context login on localstorage :sparkles:

--- Courses/Rocketseat/GoStack/Module Three/gobarber-web/src/pages/SignIn/index.tsx
+++ Courses/Rocketseat/GoStack/Module Three/gobarber-web/src/pages/SignIn/index.tsx
@@ -18,9 +18,9 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { signIn } = useContext(AuthContext);
+  const { user, signIn } = useContext(AuthContext);
 
-  console.log(signIn);
+  console.log(user);
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
