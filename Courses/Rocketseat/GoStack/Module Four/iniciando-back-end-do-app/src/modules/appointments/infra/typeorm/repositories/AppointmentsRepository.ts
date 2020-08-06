8c76cdd73779ea68e01df05359a6c3935804d9b4
caiohenrique-developer <caiorussellrox@msn.com>
2020-08-06 20:39:40 -0300

    [FEAT]: Liskov Substitution Principle :sparkles:

--- Courses/Rocketseat/GoStack/Module Four/iniciando-back-end-do-app/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ Courses/Rocketseat/GoStack/Module Four/iniciando-back-end-do-app/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,16 +1,18 @@
 import { EntityRepository, Repository } from 'typeorm';
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
 // This guy is responsable to manager all process
 // of my Appointment route, as create, delete, list, edit.
 @EntityRepository(Appointment)
-class AppointmentsRepository extends Repository<Appointment> {
-	public async findByDate(date: Date): Promise<Appointment | null> {
+class AppointmentsRepository extends Repository<Appointment>
+	implements IAppointmentsRepository {
+	public async findByDate(date: Date): Promise<Appointment | undefined> {
 		const findAppointment = await this.findOne({
 			where: { date },
 		});
 
-		return findAppointment || null;
+		return findAppointment;
 	}
 }
 
