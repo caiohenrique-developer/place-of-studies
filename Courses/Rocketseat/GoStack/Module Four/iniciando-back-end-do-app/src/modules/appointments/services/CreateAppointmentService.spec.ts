9d3ba8fe46a363d27d3c9f7e1642913509f071e1
caiohenrique-developer <caiorussellrox@msn.com>
2020-08-10 21:10:55 -0300

    [FEAT]: creating the tests of my application :sparkles:

--- Courses/Rocketseat/GoStack/Module Four/iniciando-back-end-do-app/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ Courses/Rocketseat/GoStack/Module Four/iniciando-back-end-do-app/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,9 @@
-test('Sum two numbers', () => {
-	expect(8 + 8).toBe(16);
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+	it('should be able to create a new appointment', () => {});
+
+	// it('should not be able to create two appointments on the same time', () => {
+	// 	expect(3 + 3).toBe(6);
+	// });
 });
