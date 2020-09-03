3d15ca1975b82fca3012bc0b0ea3ed6ebeafed30
caiohenrique-developer <caiorussellrox@msn.com>
2020-09-03 20:44:32 -0300

    [FEAT]: listing available hours :sparkles:

--- Courses/Rocketseat/GoStack/Module Four/iniciando-back-end-do-app/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ Courses/Rocketseat/GoStack/Module Four/iniciando-back-end-do-app/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -3,6 +3,7 @@ import IAppointmentsRepository from '@modules/appointments/repositories/IAppoint
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProviderDTO from '@modules/appointments/dtos/IFindAllInDayFromProviderDTO';
 
 // This guy is responsable to manager all process
 // of my Appointment route, as create, delete, list, edit.
@@ -34,6 +35,28 @@ class AppointmentsRepository implements IAppointmentsRepository {
 				date: Raw(
 					dateFieldName =>
 						`to_char(${dateFieldName}, 'MM-YYYY') = '${parsedMonth}-${year}'`,
+				),
+			},
+		});
+
+		return appointments;
+	}
+
+	public async findAllInDayFromProvider({
+		provider_id,
+		day,
+		month,
+		year,
+	}: IFindAllInDayFromProviderDTO): Promise<Appointment[]> {
+		const parsedDay = String(day).padStart(2, '0');
+		const parsedMonth = String(month).padStart(2, '0');
+
+		const appointments = await this.ormRepository.find({
+			where: {
+				provider_id,
+				date: Raw(
+					dateFieldName =>
+						`to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
 				),
 			},
 		});
