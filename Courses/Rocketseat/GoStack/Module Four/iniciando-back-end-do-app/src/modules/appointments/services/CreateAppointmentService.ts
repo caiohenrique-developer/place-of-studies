227c8452d248613b30e0a4edfb81f154fe8f5388
caiohenrique-developer <caiorussellrox@msn.com>
2020-09-04 19:46:21 -0300

    [DOC]: shedules rules done but i need to correct the "can not create two appointments..." test :sparkles:

--- Courses/Rocketseat/GoStack/Module Four/iniciando-back-end-do-app/src/modules/appointments/services/CreateAppointmentService.ts
+++ Courses/Rocketseat/GoStack/Module Four/iniciando-back-end-do-app/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour } from 'date-fns';
+import { startOfHour, isBefore, getHours } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
@@ -25,12 +25,26 @@ class CreateAppointmentService {
 	}: IRequestDTO): Promise<Appointment> {
 		const appointmentDate = startOfHour(date);
 
+		if (isBefore(appointmentDate, Date.now())) {
+			throw new AppError("You can't to create an appointment on a past date.");
+		}
+
+		if (user_id === provider_id) {
+			throw new AppError("You can't to create an appointmente with yourself.");
+		}
+
+		if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+			throw new AppError(
+				'You can only create appointments between 8am and on the max 17pm.',
+			);
+		}
+
 		const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
 			appointmentDate,
 		);
 
 		if (findAppointmentInSameDate) {
-			throw new AppError('This appointment is already book');
+			throw new AppError('This appointment is already booked');
 		}
 
 		const appointment = await this.appointmentsRepository.create({
