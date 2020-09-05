cab80e89090218436256a6e105e3e97aadc719bb
caiohenrique-developer <caiorussellrox@msn.com>
2020-09-05 15:01:52 -0300

    [FEAT]: provider schedule done :sparkles:

---
+++ Courses/Rocketseat/GoStack/Module Four/iniciando-back-end-do-app/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -0,0 +1,39 @@
+import { injectable, inject } from 'tsyringe';
+
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+
+interface IRequestDTO {
+	provider_id: string;
+	month: number;
+	year: number;
+	day: number;
+}
+
+@injectable()
+class ListProviderAppointmentsService {
+	constructor(
+		@inject('AppointmentsRepository')
+		private appointmentsRepository: IAppointmentsRepository,
+	) {}
+
+	public async execute({
+		provider_id,
+		month,
+		year,
+		day,
+	}: IRequestDTO): Promise<Appointment[]> {
+		const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+			{
+				provider_id,
+				month,
+				year,
+				day,
+			},
+		);
+
+		return appointments;
+	}
+}
+
+export default ListProviderAppointmentsService;
--- Courses/Rocketseat/GoStack/Module Four/iniciando-back-end-do-app/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
+++ Courses/Rocketseat/GoStack/Module Four/iniciando-back-end-do-app/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -87,10 +87,10 @@ describe('ListProviderMonthAvailability', () => {
 
 		expect(availability).toEqual(
 			expect.arrayContaining([
-				{ day: 19, availability: true },
-				{ day: 20, availability: false },
-				{ day: 21, availability: true },
-				{ day: 22, availability: true },
+				{ day: 19, available: true },
+				{ day: 20, available: false },
+				{ day: 21, available: true },
+				{ day: 22, available: true },
 			]),
 		);
 	});
