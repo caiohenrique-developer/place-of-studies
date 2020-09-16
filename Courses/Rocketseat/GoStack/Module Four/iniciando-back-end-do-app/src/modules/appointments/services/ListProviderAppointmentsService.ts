5f99f1cf8ceddb7841f88494998512b605ca52d4
caiohenrique-developer <caiorussellrox@msn.com>
2020-09-15 22:14:42 -0300

    [FEAT]: configuring cache with redis lib :sparkles:

--- Courses/Rocketseat/GoStack/Module Four/iniciando-back-end-do-app/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ Courses/Rocketseat/GoStack/Module Four/iniciando-back-end-do-app/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -3,6 +3,8 @@ import { injectable, inject } from 'tsyringe';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+
 interface IRequestDTO {
 	provider_id: string;
 	month: number;
@@ -15,6 +17,9 @@ class ListProviderAppointmentsService {
 	constructor(
 		@inject('AppointmentsRepository')
 		private appointmentsRepository: IAppointmentsRepository,
+
+		@inject('CacheProvider')
+		private cacheProvider: ICacheProvider,
 	) {}
 
 	public async execute({
@@ -23,6 +28,10 @@ class ListProviderAppointmentsService {
 		year,
 		day,
 	}: IRequestDTO): Promise<Appointment[]> {
+		const data = await this.cacheProvider.recover('blz');
+
+		console.log(data); // beleza
+
 		const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
 			{
 				provider_id,
@@ -31,6 +40,8 @@ class ListProviderAppointmentsService {
 				day,
 			},
 		);
+
+		// await this.cacheProvider.save('blz', 'beleza');
 
 		return appointments;
 	}
