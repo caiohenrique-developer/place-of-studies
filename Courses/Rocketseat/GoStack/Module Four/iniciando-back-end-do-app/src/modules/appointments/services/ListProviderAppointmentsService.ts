1430f0804bd76bf614933a420dc3f07f55ad67f6
caiohenrique-developer <caiorussellrox@msn.com>
2020-09-17 21:27:04 -0300

    [DOC]: developing cache to appointments :sparkles:

--- Courses/Rocketseat/GoStack/Module Four/iniciando-back-end-do-app/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ Courses/Rocketseat/GoStack/Module Four/iniciando-back-end-do-app/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -28,20 +28,24 @@ class ListProviderAppointmentsService {
 		year,
 		day,
 	}: IRequestDTO): Promise<Appointment[]> {
-		const data = await this.cacheProvider.recover('blz');
+		const cacheKey = `provider-appointments:${provider_id}:${month}:${year}:${day}`;
 
-		console.log(data); // beleza
-
-		const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
-			{
-				provider_id,
-				month,
-				year,
-				day,
-			},
+		let appointments = await this.cacheProvider.recover<Appointment[]>(
+			cacheKey,
 		);
 
-		// await this.cacheProvider.save('blz', 'beleza');
+		if (!appointments) {
+			appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+				{
+					provider_id,
+					month,
+					year,
+					day,
+				},
+			);
+
+			await this.cacheProvider.save(cacheKey, appointments);
+		}
 
 		return appointments;
 	}
