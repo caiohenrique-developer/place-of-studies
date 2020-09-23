c0c40050847c31d953bc7c6e17a701461e782eaa
caiohenrique-developer <caiorussellrox@msn.com>
2020-09-23 20:54:17 -0300

    [DOC]: cache serialized :sparkles:

--- Courses/Rocketseat/GoStack/Module Four/iniciando-back-end-do-app/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ Courses/Rocketseat/GoStack/Module Four/iniciando-back-end-do-app/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,10 +1,10 @@
 import { injectable, inject } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
-import { classToClass } from 'class-transformer';
 
 interface IRequestDTO {
 	provider_id: string;
@@ -45,7 +45,7 @@ class ListProviderAppointmentsService {
 				},
 			);
 
-			await this.cacheProvider.save(cacheKey, appointments);
+			await this.cacheProvider.save(cacheKey, classToClass(appointments));
 		}
 
 		return appointments;
