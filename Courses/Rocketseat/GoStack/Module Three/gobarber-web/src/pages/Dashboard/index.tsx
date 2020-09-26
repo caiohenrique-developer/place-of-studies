9d5fde216a36c849d8b8be8cecf20fc685874a7f
caiohenrique-developer <caiorussellrox@msn.com>
2020-09-25 21:34:37 -0300

    [FEAT]: header front-end created :sparkles:

--- Courses/Rocketseat/GoStack/Module Three/gobarber-web/src/pages/Dashboard/index.tsx
+++ Courses/Rocketseat/GoStack/Module Three/gobarber-web/src/pages/Dashboard/index.tsx
@@ -1,5 +1,37 @@
 import React from 'react';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import { FiPower } from 'react-icons/fi';
+import { Container, Header, HeaderContent, Profile } from './styles';
+import { useAuth } from '../../hooks/auth';
+
+import logoImg from '../../assets/logo.svg';
+
+const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+  const { avatar_url, name } = user;
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="Logotipo GoBarber" />
+
+          <Profile>
+            <img src={avatar_url} alt={name} />
+
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>{name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
+};
 
 export default Dashboard;
