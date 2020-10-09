6eb58aa577bedbca019f523c5bbeaf033760badc
caiohenrique-developer <caiorussellrox@msn.com>
2020-10-08 22:01:16 -0300

    [FEAT]: listing providers from api :sparkles:

--- Courses/Rocketseat/GoStack/Module Three/gobarber-mob/src/pages/Dashboard/index.tsx
+++ Courses/Rocketseat/GoStack/Module Three/gobarber-mob/src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import { useNavigation } from '@react-navigation/native';
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import api from '../../services/api';
 import { useAuth } from '../../hooks/auth';
 import {
   Container,
@@ -8,15 +9,30 @@ import {
   UserName,
   ProfileButton,
   UserAvatar,
+  ProvidersList,
 } from './styles';
 
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
 const Dashboard: React.FC = () => {
+  const [providers, setProviders] = useState<Provider[]>([]);
   const { signOut, user } = useAuth();
   const { navigate } = useNavigation();
 
+  useEffect(() => {
+    api.get('providers').then(response => {
+      setProviders(response.data);
+    });
+  }, []);
+
   const navigateToProfile = useCallback(() => {
-    navigate('Profile');
-  }, [navigate]);
+    // navigate('Profile');
+    signOut();
+  }, [signOut]);
 
   return (
     <Container>
@@ -31,6 +47,12 @@ const Dashboard: React.FC = () => {
           <UserAvatar source={{ uri: user.avatar_url }} />
         </ProfileButton>
       </Header>
+
+      <ProvidersList
+        data={providers}
+        keyExtractor={provider => provider.id}
+        renderItem={({ item }) => <UserName>{item.name}</UserName>}
+      />
     </Container>
   );
 };
